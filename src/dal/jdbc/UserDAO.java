a504270422645e68219350cade6a64cd996c057b
hippothomas <hippolyte.thomas1@gmail.com>
2019-10-25 16:07:18 +0200

    Ajout des DAO

--- src/dal/jdbc/UserDAO.java
+++ src/dal/jdbc/UserDAO.java
@@ -1,19 +1,25 @@
 package dal.jdbc;
 
+import bo.Game;
 import bo.User;
 import dal.DAOFactory;
 import dal.IUserDAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAO implements IUserDAO<Long, User> {
-	
+
 	private static final String AUTHENT_QUERY = "SELECT * FROM user WHERE login = ? AND password = ?";
-	
+	private static final String INSERT_QUERY = "INSERT INTO user (login, password, connections) VALUES(?,?,?)";
+	private static final String UPDATE_QUERY = "UPDATE user SET login = ?, password = ?, connections = ? WHERE id = ?";
+	private static final String REMOVE_QUERY = "DELETE FROM user WHERE id = ?";
+	private static final String FIND_QUERY = "SELECT * FROM user WHERE id = ?";
+	private static final String FIND_ALL_QUERY = "SELECT * FROM user";
+	private static final String FIND_ALL_GAME = "SELECT * FROM game WHERE id = ?";
+	private static final String FIND_BEST_GAME = "SELECT game.*, user.id AS user FROM game INNER JOIN user ON game.id_user = user.id WHERE id = ? ORDER BY score LIMIT 1";
+
 	@Override
 	public User authenticate(String login, String password ) throws SQLException {
 		Connection connection = DAOFactory.getJDBCConnection();
@@ -38,29 +44,132 @@ public class UserDAO implements IUserDAO<Long, User> {
 		}
 		return user;
 	}
-	
+
 	@Override
-	public void create( User object ) throws SQLException {
-	
+	public void create( User user ) throws SQLException {
+		Connection connection = DAOFactory.getJDBCConnection();
+		if (connection != null) {
+			try (PreparedStatement ps = connection
+					.prepareStatement(INSERT_QUERY, Statement.RETURN_GENERATED_KEYS)) {
+				ps.setString(2, user.getLogin());
+				ps.setString(3, user.getPassword());
+				ps.setInt(4, user.getConnections());
+				ps.executeUpdate();
+				try (ResultSet rs = ps.getGeneratedKeys()) {
+					if (rs.next()) {
+						user.setId(rs.getInt(1));
+					}
+				}
+			}
+		}
 	}
-	
+
 	@Override
-	public void update( User object ) throws SQLException {
-	
+	public void update( User user ) throws SQLException {
+		Connection connection = DAOFactory.getJDBCConnection();
+		if (connection != null) {
+			try (PreparedStatement ps = connection.prepareStatement(UPDATE_QUERY)) {
+				ps.setString(1, user.getLogin());
+				ps.setString(2, user.getPassword());
+				ps.setInt(3, user.getConnections());
+				ps.setInt(4, user.getId());
+				ps.executeUpdate();
+			}
+		}
 	}
-	
+
 	@Override
-	public void delete( User object ) throws SQLException {
-	
+	public void delete( User user ) throws SQLException {
+		Connection connection = DAOFactory.getJDBCConnection();
+		if (connection != null) {
+			try (PreparedStatement ps = connection.prepareStatement(REMOVE_QUERY)) {
+				ps.setInt(1, user.getId());
+				ps.executeUpdate();
+			}
+		}
 	}
-	
+
 	@Override
-	public User findById(Long aLong ) throws SQLException {
-		return null;
+	public User findById( Long aLong ) throws SQLException {
+		int id = Math.toIntExact(aLong);
+		User user = null;
+		Connection connection = DAOFactory.getJDBCConnection();
+		if (connection != null) {
+			try (PreparedStatement ps = connection.prepareStatement(FIND_QUERY)) {
+				ps.setInt(1, id);
+				try (ResultSet rs = ps.executeQuery()) {
+					if (rs.next()) {
+						user = new User();
+						user.setId(rs.getInt("id"));
+						user.setLogin(rs.getString("login"));
+						user.setPassword(rs.getString("password"));
+						user.setConnections(rs.getInt("connections"));
+					}
+				}
+			}
+		}
+		return user;
 	}
-	
+
 	@Override
 	public List<User> findByAll() throws SQLException {
-		return null;
+		List<User> list = new ArrayList<>();
+		Connection connection = DAOFactory.getJDBCConnection();
+		if (connection != null) {
+			try (PreparedStatement ps = connection.prepareStatement(FIND_ALL_QUERY)) {
+				try (ResultSet rs = ps.executeQuery()) {
+					while (rs.next()) {
+						User user = new User();
+						user.setId(rs.getInt("id"));
+						user.setLogin(rs.getString("login"));
+						user.setPassword(rs.getString("password"));
+						user.setConnections(rs.getInt("connections"));
+
+						list.add(user);
+					}
+				}
+			}
+		}
+		return list;
+	}
+
+	public Game findBestGame(Integer id) throws SQLException {
+		Game game = null;
+		Connection connection = DAOFactory.getJDBCConnection();
+		if (connection != null) {
+			try (PreparedStatement ps = connection.prepareStatement(FIND_BEST_GAME)) {
+				ps.setInt(1, id);
+				try (ResultSet rs = ps.executeQuery()) {
+					while (rs.next()) {
+						game = new Game();
+						game.setId(rs.getInt("id"));
+						game.setScore(rs.getInt("score"));
+						game.setUser(this.findById((long) rs.getInt("id_user")));
+					}
+				}
+			}
+		}
+		return game;
+	}
+
+	public List<Game> findAllGame(Integer id) throws SQLException {
+		List<Game> list = new ArrayList<>();
+		Connection connection = DAOFactory.getJDBCConnection();
+		if (connection != null) {
+			try (PreparedStatement ps = connection.prepareStatement(FIND_ALL_GAME)) {
+				ps.setInt(1, id);
+				try (ResultSet rs = ps.executeQuery()) {
+					while (rs.next()) {
+						Game game = new Game();
+						game.setId(rs.getInt("id"));
+						game.setScore(rs.getInt("score"));
+						game.setUser(this.findById((long) rs.getInt("id_user")));
+
+						list.add(game);
+					}
+				}
+			}
+		}
+		return list;
 	}
 }
