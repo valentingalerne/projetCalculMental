c2d295b8440f3b94423ac3cdbf74b8eea917eeb6
Valentin Galerne <valentin.galerne@gmail.com>
2019-11-01 10:16:06 +0100

    Mise à jour fichier SQL, script de peuplement de la BDD

--- projet-calcul.sql
+++ projet-calcul.sql
@@ -1,49 +1,44 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 23 oct. 2019 à 14:06
--- Version du serveur :  5.7.23
--- Version de PHP :  5.6.38
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `projet-calcul`
---
-CREATE DATABASE IF NOT EXISTS `projet-calcul` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `projet-calcul`;
-
+-- --------------------------------------------------------
+-- Hôte :                        127.0.0.1
+-- Version du serveur:           5.7.23 - MySQL Community Server (GPL)
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             10.2.0.5599
 -- --------------------------------------------------------
 
---
--- Structure de la table `game`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Listage de la structure de la base pour projet-calcul
+DROP DATABASE IF EXISTS `projet-calcul`;
+CREATE DATABASE IF NOT EXISTS `projet-calcul` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `projet-calcul`;
+
+-- Listage de la structure de la table projet-calcul. game
 DROP TABLE IF EXISTS `game`;
 CREATE TABLE IF NOT EXISTS `game` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) NOT NULL,
   `score` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Listage des données de la table projet-calcul.game : 0 rows
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+INSERT INTO `game` (`id`, `id_user`, `score`) VALUES
+	(1, 2, 2),
+	(2, 1, 4),
+	(3, 1, 3),
+	(4, 3, 9),
+	(5, 3, 8),
+	(6, 2, 7),
+	(7, 2, 10);
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
 
---
--- Structure de la table `user`
---
-
+-- Listage de la structure de la table projet-calcul. user
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -51,9 +46,17 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(255) NOT NULL,
   `connections` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-COMMIT;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
+-- Listage des données de la table projet-calcul.user : 0 rows
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `login`, `password`, `connections`) VALUES
+	(1, 'user1', 'pwd1', 0),
+	(2, 'user2', 'pwd2', 0),
+	(3, 'user3', 'pwd3', 0),
+	(4, 'usertest', 'usertest', 0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
